f421ffb6cbd93bae1ac56fe0ae45f1d2f8ded3ba
HellSec <57877984+rpie@users.noreply.github.com>
2021-08-03 12:54:00 -0900

    Delete iris_modules directory

--- iris_modules/twitteremail.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def run(target):
-    if '@' not in target:
-        print(f'    {Fore.RED}•{Fore.RESET} Non-Email Skipping Module')
-        return
-
-    endpoint = 'https://api.twitter.com/i/users/email_available.json?email={target}&send_error_codes=1'
